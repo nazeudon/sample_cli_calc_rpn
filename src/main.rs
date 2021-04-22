81f46615f61c611ebf34d8f4041438b3263a7e81
nazeudon <nazeudon48112@icloud.com>
2021-04-22 22:48:20 +0900

    add file reader

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Clap;
-//use clap::{App, Arg};
-// use std::env;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
 
 #[derive(Clap, Debug)]
 #[clap(
@@ -21,41 +21,21 @@ struct Opts {
 
 fn main() {
     let opts = Opts::parse();
-    match opts.formula_file {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified."),
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose)
+    } else {
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
     }
-    println!("Is verbosity specified?: {}", opts.verbose);
+}
 
-    // not using third party library.
-    // let args: Vec<String> = env::args().collect();
-    // println!("{:?}", args);
-
-    // using Builder pattern
-    // let matches = App::new("My RPN program")
-    //     .version("1.0.0")
-    //     .author("nazeudon")
-    //     .about("Super awsome sample RPN calculator")
-    //     .arg(
-    //         Arg::new("formula_file")
-    //             .about("Formula written in RPN")
-    //             .value_name("FILE")
-    //             .index(1)
-    //             .required(false),
-    //     )
-    //     .arg(
-    //         Arg::new("verbose")
-    //             .about("Sets the level of verbosity")
-    //             .short('v')
-    //             .long("verbose")
-    //             .required(false),
-    //     )
-    //     .get_matches();
-
-    // match matches.value_of("formula_file") {
-    //     Some(file) => println!("File specified: {}", file),
-    //     None => println!("No file specified."),
-    // }
-    // let verbose = matches.is_present("verbose");
-    // println!("Is verbosity specified?: {}", verbose);
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
 }
